0f8cbf0195bc40bb0895d2819a8537d50dfd9b70
a20caros <a20caros@student.his.se>
2021-04-26 11:02:22 +0200

    Lagt till en arraylist och en arrayadapter i MainActivity.java

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<Mountain> mountainArrayList=new ArrayList<>();
+    ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this, R.layout.list_textview, R.id.list_textview_xml,mountainArrayList);
+    )
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
