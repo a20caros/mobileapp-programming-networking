2655cdab822845c72f2852f4db33f8dbed13ff38
a20caros <a20caros@student.his.se>
2021-04-26 11:13:22 +0200

    Lagt till kod så att den kan läsa innehåll i en fil med hjälp av string variabler samt lagt in kod i oncreate för att filen enbart skall läsas en gång. Detta gjordes i MainActivity.java

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,18 +3,40 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Mountain> mountainArrayList=new ArrayList<>();
     ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this, R.layout.list_textview, R.id.list_textview_xml,mountainArrayList);
-    )
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            InputStream is = getApplicationContext().getAssets().open("mountains.json");
+            String s = convertStreamToString(is);
+            Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+        }catch (Exception e){
+            Log.e("MainActivity","Something went wrong when reading textfile:\n\n"+ e.getMessage());
+        }
     }
 }
